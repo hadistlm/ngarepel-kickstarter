cd583b8d2395238b05f1228e8898541052872afa
Islaa <hadist.lm@gmail.com>
2019-09-18 13:51:22 +0700

    Adding: setting table migrations

---
+++ database/migrations/2019_09_18_064939_create_setting_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSettingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('setting', function (Blueprint $table) {
+            $table->bigIncrements('setting_id');
+            $table->string('setting_name', 50)->nullable();
+            $table->text('setting_value')->nullable();
+            $table->string('label', 50)->nullable();
+            $table->string('category', 50)->nullable();
+            $table->integer('user_id')->nullable();
+            $table->timestamps();
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('setting');
+    }
+}
