30d372e872534fde106be53b9e24a651988480fc
Islaa <hadist.lm@gmail.com>
2020-01-06 09:49:29 +0700

    Adding: Helper new functions

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -21,6 +21,76 @@ if (! function_exists('get_option')) {
 	}
 }
 
+if (! function_exists('get_user_data')) {
+  function get_user_data($user_id = null){
+    if (empty($user_id)) return false;
+
+    $data = \App\User::find($user_id)->first();
+
+    if (!empty($data)) :
+      return $data;
+    else:
+      return false;
+    endif;
+  }
+}
+
+if (! function_exists('toSlug')) {
+  function toSlug($text){
+    // replace non letter or digits by -
+    $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+    // transliterate
+    $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+    // remove unwanted characters
+    $text = preg_replace('~[^-\w]+~', '', $text);
+    // trim
+    $text = trim($text, '-');
+    // remove duplicate -
+    $text = preg_replace('~-+~', '-', $text);
+    // lowercase
+    $text = strtolower($text);
+
+    if (empty($text)) {
+      return 'n-a';
+    }
+
+    return $text;
+  }
+}
+
+if (! function_exists('controller_lists')) {
+    function controller_lists($nameOnly = true)
+    {
+        $controllers = array();
+
+        foreach (\Illuminate\Support\Facades\Route::getRoutes()->getRoutes() as $route)
+        {
+            $action = $route->getAction();
+
+            if (array_key_exists('controller', $action))
+            {
+                // explode the string with @ creating an array with a count of 2
+                $explodedAction = explode('@', $action['controller']);
+                // explode the string with Controllers
+                $controllerRoute = explode("Controllers\\", $explodedAction[0]);
+                // check to see if an array exists for the controller name
+                if (!in_array($controllerRoute[1], $controllers) && strpos($controllerRoute[1], 'Auth') !== 0) :
+                    $controllers[] = $controllerRoute[1];
+                endif;
+            }
+        }
+
+        // if user request to only return the name
+        if ($nameOnly == true) :
+            $controllers = array_map(function($val) { 
+                return str_replace("Controller", "", $val); 
+            }, $controllers);
+        endif;
+
+        return $controllers;
+    }
+}
+
 if (! function_exists('limit_words')) {
     /**
      * Limit the number of words in a string.
@@ -33,5 +103,40 @@ if (! function_exists('limit_words')) {
     function limit_words($value, $words = 100, $end = '...')
     {
         return \Illuminate\Support\Str::words($value, $words, $end);
+    }
+}
+
+if (! function_exists('formatBytes')) {
+    /**
+     * return bytes from integer into string word
+     * @source https://www.codercrunch.com/question/727833297/how-convert-bytes-human-readable-string-format-php
+     *
+     * @param  int  $bytes
+     * @param  int  $format
+     * @return string
+     */
+    function formatBytes($bytes, $format = 99) {
+        $byte_size = 1024;
+        $byte_type = array(" KB", " MB", " GB", " TB", " PB", " EB", " ZB", " YB");
+     
+        $bytes /= $byte_size;
+        $i = 0;
+     
+        if ($format == 99 || $format > 7) {
+          while ($bytes > $byte_size) {
+                $bytes /= $byte_size;
+                $i++;
+            }
+        } else {
+          while ($i < $format) {
+                $bytes /= $byte_size;
+                $i++;
+            }
+        }
+     
+        $bytes = sprintf("%1.2f", $bytes);
+        $bytes .= $byte_type[$i];
+     
+        return$bytes;
     }
 }
